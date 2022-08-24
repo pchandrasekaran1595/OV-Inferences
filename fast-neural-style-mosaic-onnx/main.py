2f88d982df8abd2128c2208ea38a09e0c69620a2
pchandrasekaran1595 <jackstapleton920@gmail.com>
2022-08-24 22:39:09 +0300

    Added fast-neural-style-mosaic-onnx

---
+++ fast-neural-style-mosaic-onnx/main.py
@@ -0,0 +1,163 @@
+import os
+import re
+import sys
+import cv2
+import json
+import platform
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from typing import Optional
+from openvino.runtime import Core
+
+
+REPO_PATH: str = f"{os.environ['BUILD_DISK']}/Repos/OV-Inferences"
+MODEL_PATH=f"{os.environ['BUILD_DISK']}/OVM/ir/public/"
+
+INPUT_PATH: str = os.path.join(REPO_PATH, "input")
+LABEL_PATH: str = os.path.join(REPO_PATH, "labels")
+MODEL_NAME: str = os.path.dirname(os.path.realpath(__file__)).split("\\")[-1]
+
+ID: int = 0
+CAM_WIDTH: int  = 640
+CAM_HEIGHT: int = 360 
+FPS: int = 30
+
+MEAN_VAL_R: float = 123.68
+MEAN_VAL_G: float = 116.779
+MEAN_VAL_B: float = 103.939
+
+
+def breaker(num: int=50, char: str="*") -> None:
+    print("\n" + num*char + "\n")
+
+
+def preprocess(image: np.ndarray, width: int, height: int) -> np.ndarray:
+    image = cv2.resize(src=image, dsize=(width, height), interpolation=cv2.INTER_AREA).transpose(2, 0, 1)
+    return np.expand_dims(image, axis=0)
+
+
+def show_image(
+    image: np.ndarray, 
+    cmap: Optional[str]="gnuplot2", 
+    title: Optional[str]=None
+    ) -> None:
+
+    plt.figure()
+    plt.imshow(image, cmap=cmap)
+    if title: plt.title(title)
+    figmanager = plt.get_current_fig_manager()
+    figmanager.window.state("zoomed")
+    plt.show()
+
+
+def setup(target: str) -> tuple:
+    ie = Core()
+    model = ie.read_model(model=f"{MODEL_PATH}/{MODEL_NAME}/FP16/{MODEL_NAME}.xml")
+    model = ie.compile_model(model=model, device_name=target)
+
+    input_layer = next(iter(model.inputs))
+    output_layer = next(iter(model.outputs))
+
+    return model, input_layer, output_layer, \
+           (input_layer.shape[0], input_layer.shape[1], input_layer.shape[2], input_layer.shape[3])
+
+
+def infer(
+    model, 
+    output_layer, 
+    image: np.ndarray, 
+    w: int, 
+    h: int,
+    negative: bool
+) -> np.ndarray:
+
+    result = model(inputs=[image])[output_layer].squeeze().transpose(1, 2, 0)
+    result = result[::] + (MEAN_VAL_R, MEAN_VAL_G, MEAN_VAL_B)
+    result = cv2.resize(src=result, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
+
+    result[result < 0] = 0
+    result[result > 255] = 255
+
+    if negative: result = 255 - result
+
+    return result / 255
+    
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", "-m", type=str, default="image", help="Mode: image or video or realtime")
+    parser.add_argument("--filename", "-f", type=str, default="Test_1.jpg", help="Image or Video Filename")
+    parser.add_argument("--downscale", "-ds", type=float, default=None, help="Downscale factor (Useful for Videos)")
+    parser.add_argument("--target", "-t", type=str, default="CPU", help="Target Device for Inference")
+    parser.add_argument("--negative", "-n", action="store_true", help="Negate the result")
+    args = parser.parse_args()
+
+    assert args.filename in os.listdir(INPUT_PATH), "File not Found"
+
+    model, input_layer, output_layer, (N, C, H, W) = setup(args.target)
+
+    if re.match(r"^image$", args.mode, re.IGNORECASE):
+        image = cv2.imread(os.path.join(INPUT_PATH, args.filename), cv2.IMREAD_COLOR)
+        h, w, _ = image.shape
+        image = preprocess(image, W, H)
+        inferred_image = infer(model, output_layer, image, w, h, args.negative)
+
+        show_image(inferred_image)
+
+    elif re.match(r"^video$", args.mode, re.IGNORECASE):
+        cap = cv2.VideoCapture(os.path.join(INPUT_PATH, args.filename))
+
+        while True:
+            ret, frame = cap.read()
+            if ret:
+                if args.downscale:
+                    frame = cv2.resize(
+                        src=frame, 
+                        dsize=(int(frame.shape[1]/args.downscale), int(frame.shape[0]/args.downscale)), 
+                        interpolation=cv2.INTER_AREA
+                    )
+                
+                h, w, _ = frame.shape
+                frame = preprocess(frame, W, H)
+                frame = infer(model, output_layer, frame, w, h, args.negative)
+
+                cv2.imshow("Feed", frame)
+            else:
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+    
+    elif re.match(r"^realtime$", args.mode, re.IGNORECASE):
+        if platform.system() != "Windows":
+            cap = cv2.VideoCapture(ID)
+        else:
+            cap = cv2.VideoCapture(ID, cv2.CAP_DSHOW)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAM_WIDTH)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAM_HEIGHT)
+        cap.set(cv2.CAP_PROP_FPS, FPS)
+
+        while True:
+            ret, frame = cap.read()
+            if not ret: break
+            
+            frame = preprocess(frame, W, H)
+            frame = infer(model, output_layer, frame, CAM_WIDTH, CAM_HEIGHT, args.negative)
+
+            cv2.imshow("Feed", frame)
+        
+            if cv2.waitKey(1) & 0xFF == ord('q'): 
+                break
+        
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    sys.exit(main() or 0)
