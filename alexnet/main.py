ab8898661a0f19dd5d278c0ed0f2595f2fcc475c
prashanth-acsq <prashanth@acsq.com.qa>
2022-09-19 13:27:02 +0300

    Fixed assert statement

--- alexnet/main.py
+++ alexnet/main.py
@@ -71,12 +71,13 @@ def main():
     parser.add_argument("--target", "-t", type=str, default="CPU", help="Target Device for Inference")
     args = parser.parse_args()
 
-    assert args.filename in os.listdir(INPUT_PATH), "File not Found"
     assert args.target in ["CPU", "GPU"], "Invalid Target Device"
 
     model, labels, input_layer, output_layer, (N, C, H, W) = setup(args.target)
 
     if re.match(r"^image$", args.mode, re.IGNORECASE):
+        assert args.filename in os.listdir(INPUT_PATH), "File not Found"
+
         image = cv2.imread(os.path.join(INPUT_PATH, args.filename), cv2.IMREAD_COLOR)
         image = preprocess(image, W, H)
 
@@ -87,6 +88,8 @@ def main():
         breaker()
     
     elif re.match(r"^video$", args.mode, re.IGNORECASE):
+        assert args.filename in os.listdir(INPUT_PATH), "File not Found"
+
         cap = cv2.VideoCapture(os.path.join(INPUT_PATH, args.filename))
 
         while True:
--- facenet-20180408-102900/main.py
+++ facenet-20180408-102900/main.py
@@ -74,6 +74,8 @@ def main():
     model, input_layer, output_layer, (N, H, W, C) = setup(args.target)
 
     if re.match(r"^image$", args.mode, re.IGNORECASE):
+        assert args.filename in os.listdir(INPUT_PATH), "File not Found"
+
         image = cv2.imread(os.path.join(INPUT_PATH, args.filename), cv2.IMREAD_COLOR)
         image = preprocess(image, W, H)
 
